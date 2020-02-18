5869964c676e5ad54aa4b9e04a69c8d74b929ac2
Piotr Moszkowicz <piotr@moszkowicz.pl>
2020-02-18 05:33:56 +0100

    Fix all the services add AuthorizationCodeService

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,6 +1,5 @@
 import bcrypt from "bcrypt";
 
-import Token from "@models/Token";
 import User from "@models/User";
 
 import Logger from "@util/logger";
@@ -34,36 +33,6 @@ export default class UserService {
       return user;
     } catch (err) {
       Logger.log("error", "UserService getUserByLoginAndPassword error", { message: err });
-      return null;
-    }
-  }
-
-  /**
-   * Returns user by access token
-   * @param accessToken
-   */
-  public static async getUserByAccessToken(accessToken: string): Promise<User> {
-    try {
-      const userWithToken = await User.findOne({
-        include: [
-          {
-            model: Token,
-            where: {
-              accessToken,
-            },
-            required: true,
-          },
-        ],
-      });
-
-      if (!userWithToken) {
-        Logger.log("info", `The token doesn't exist (${accessToken})`);
-        return null;
-      }
-
-      return userWithToken;
-    } catch (err) {
-      Logger.log("error", "UserService getUserByAccessToken error", { message: err });
       return null;
     }
   }
