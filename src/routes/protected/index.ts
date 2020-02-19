19d01f0df16f472a03e69189f38ae11d1230c28d
Piotr Moszkowicz <piotr@moszkowicz.pl>
2020-02-20 00:08:16 +0100

    Add userChange form

--- src/routes/protected/index.ts
+++ src/routes/protected/index.ts
@@ -3,12 +3,14 @@ import { Router } from "express";
 import { isAuthenticated, isAuthenticatedAsForeman, isAuthenticatedAsAdmin } from "@middleware/auth";
 
 import dashboardRoutes from "@routes/protected/dashboard";
+import userChangeRoutes from "@routes/protected/userChange";
 
 const router = Router();
 
 // Routes which need normal authentication
 router.use(isAuthenticated);
 router.use(dashboardRoutes);
+router.use(userChangeRoutes);
 
 // Routes which need foreman / admin authentication
 router.use(isAuthenticatedAsForeman);
