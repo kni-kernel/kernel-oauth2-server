ff72d5b1a6f9d2a7a31d0b2b703ec4b6e70b600f
Piotr Moszkowicz <piotr@moszkowicz.pl>
2020-02-20 00:34:10 +0100

    [KERAUTH-10] Add account list (#31)
    * Add getting of users list with UserGetOptions typing
    
    * Add UserList route and view

---
+++ src/routes/protected/userList.ts
@@ -0,0 +1,27 @@
+import { Router } from "express";
+
+import UserService from "@services/UserService";
+
+import { RequestOverride } from "ExpressOverride";
+import UserGetOptions from "UserGetOptions";
+
+const router = Router();
+
+router.get("/dashboard/user/list", async (req: RequestOverride, res) => {
+  let opts: UserGetOptions = undefined;
+
+  if (req.user.privilege === Privilege.Foreman) {
+    opts = {
+      beggingYear: req.user.beginningYear,
+      fieldOfStudy: req.user.fieldOfStudy,
+    };
+  }
+
+  try {
+    return res.render("userList", { user: req.user, users: await UserService.getUsers(opts) });
+  } catch (err) {
+    return res.render("error", { user: req.user, error: err });
+  }
+});
+
+export default router;
