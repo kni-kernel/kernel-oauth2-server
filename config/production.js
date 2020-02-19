1867717069408d9dbce4846e5bde439d469552a7
Piotr Moszkowicz <piotr@moszkowicz.pl>
2020-02-19 17:41:40 +0100

    [KERAUTH-8] Finish README (#28)
    * Update production config
    
    * All example .env file for Docker
    
    * Add docker-compose
    
    * Add Contributing guide
    
    * Add docker guide
    
    * Add project's license
    
    * Finish README

--- config/production.js
+++ config/production.js
@@ -1,17 +1,17 @@
 module.exports = {
   database: {
-    engine: process.env.DATABASE_ENGINE,
-    host: process.env.DATABASE_HOST,
-    maxConnections: process.env.DATABASE_MAX_CONNECTIONS,
-    maxIdleTime: process.env.DATABASE_MAX_IDLE_TIME,
-    minConnections: process.env.DATABASE_MIN_CONNECTIONS,
+    engine: process.env.DATABASE_ENGINE || "postgres",
+    host: process.env.DATABASE_HOST || "127.0.0.1",
+    maxConnections: process.env.DATABASE_MAX_CONNECTIONS || 25,
+    maxIdleTime: process.env.DATABASE_MAX_IDLE_TIME || 30000,
+    minConnections: process.env.DATABASE_MIN_CONNECTIONS || 1,
     name: process.env.DATABASE_NAME,
     password: process.env.DATABASE_PASSWORD,
     port: process.env.DATABASE_PORT,
     user: process.env.DATABASE_USER,
   },
   oauth: {
-    port: process.env.OAUTH_PORT,
+    port: 5000,
     debug: false,
     dsn: process.env.OAUTH_DSN,
   },
