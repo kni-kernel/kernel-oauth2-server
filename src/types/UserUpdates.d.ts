e5749c4b0a47349adac2593bea044c0fc3b07c8e
Piotr Moszkowicz <piotr@moszkowicz.pl>
2020-02-20 01:57:24 +0100

    [KERAUTH-12] Add edit account feature (#33)
    * Some more type fixes
    
    * Add types in tsconfig
    
    * Fix in user add
    
    * Don't allow foremans to edit admins
    
    * Add editing of users

--- src/types/UserUpdates.d.ts
+++ src/types/UserUpdates.d.ts
@@ -2,4 +2,7 @@ export default interface UserUpdates {
   username: string;
   email: string;
   password?: string;
+  beginningYear?: number;
+  fieldOfStudy?: FieldOfStudy;
+  privilege?: Privilege;
 }
