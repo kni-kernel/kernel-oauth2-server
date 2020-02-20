c99b98d338a63b323e44bc7425ddcb9450240ae0
Piotr Moszkowicz <piotr@moszkowicz.pl>
2020-02-20 01:23:29 +0100

    Add add new user route and view

--- src/routes/protected/index.ts
+++ src/routes/protected/index.ts
@@ -3,6 +3,7 @@ import { Router } from "express";
 import { isAuthenticated, isAuthenticatedAsForeman, isAuthenticatedAsAdmin } from "@middleware/auth";
 
 import dashboardRoutes from "@routes/protected/dashboard";
+import userAddRoutes from "@routes/protected/userAdd";
 import userChangeRoutes from "@routes/protected/userChange";
 import userListRoutes from "@routes/protected/userList";
 
@@ -12,10 +13,11 @@ const router = Router();
 router.use(isAuthenticated);
 router.use(dashboardRoutes);
 router.use(userChangeRoutes);
-router.use(userListRoutes);
 
 // Routes which need foreman / admin authentication
 router.use(isAuthenticatedAsForeman);
+router.use(userAddRoutes);
+router.use(userListRoutes);
 
 // Routes which need admin authentication
 router.use(isAuthenticatedAsAdmin);
